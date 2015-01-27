9f98cc65839277c084c6d015221db0d2f25bbf46
Carl1101 <carlosvazquez014@gmail.com>
2015-01-27 15:16:27 -0400

    Created Project

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name carl1101githubioApp
+ * @description
+ * # carl1101githubioApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('carl1101githubioApp', [
+    'ngAnimate'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name carl1101githubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the carl1101githubioApp
+ */
+angular.module('carl1101githubioApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
